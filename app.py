e9c86614e15451018a06ce5a83fd04d1806b9ada
Anjelica <ambrosio.anjelica@gmail.com>
2019-12-03 16:03:07 -0800

    Testing get_default_database

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
-db = client.Playlister
+# db = client.Playlister
+db = client.get_default_database()
 playlists = db.playlists
 comments = db.comments
 
